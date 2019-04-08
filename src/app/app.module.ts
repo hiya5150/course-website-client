a2fd51b07a6269df2a252d0e25538901c00eeb65
Admin <5828537@gmail.com>
2019-04-08 15:12:06 -0400

    file structure almost complete(besides student components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './views/teachers/home/home.component';
+import { AssignmentsComponent } from './views/teachers/assignments/assignments.component';
+import { AnnouncementsComponent } from './views/teachers/announcements/announcements.component';
+import { GradesComponent } from './views/teachers/grades/grades.component';
+import { NavbarComponent } from './views/main/navbar/navbar.component';
+import { LoginComponent } from './views/main/login/login.component';
+import { RegisterComponent } from './views/main/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AssignmentsComponent,
+    AnnouncementsComponent,
+    GradesComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
