9095ef9df6118e7f2f32aa444efc50d3980a8fbf
Lesches <sholomls@gmail.com>
2019-04-09 16:16:44 -0400

    beginning assignment component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RegisterComponent } from './views/main/register/register.component';
-import {MatInputModule, MatSelectModule, MatStepperModule} from '@angular/material';
+import {
+  MatInputModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatSelectModule,
+  MatStepperModule,
+  MatTableModule
+} from '@angular/material';
 import {FormsModule} from '@angular/forms';
+import { GradesComponent } from './views/students/grades/grades.component';
+import { StudentHomepageComponent } from './views/students/student-homepage/student-homepage.component';
+import { AssignmentsComponent } from './views/students/assignments/assignments.component';
+import { AnnouncementsComponent } from './view/students/announcements/announcements.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    RegisterComponent,
+    GradesComponent,
+    StudentHomepageComponent,
+    AssignmentsComponent,
+    AnnouncementsComponent
   ],
   imports: [
     BrowserModule,
@@ -20,7 +35,10 @@ import {FormsModule} from '@angular/forms';
     MatStepperModule,
     MatSelectModule,
     MatInputModule,
-    FormsModule
+    FormsModule,
+    MatTableModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
