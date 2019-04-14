b0a1c1c991c02f952bbd6891529ea829c24e2546
malkie <malkieg970@gmail.com>
2019-04-14 14:07:29 -0400

    view single assignment works

--- src/app/views/teachers/assignment/teachers-assignment.component.ts
+++ src/app/views/teachers/assignment/teachers-assignment.component.ts
@@ -16,11 +16,18 @@ export class TeachersAssignmentComponent implements OnInit {
   asnTitle;
   asnBody;
   asnDueDate;
+  asnGrade;
+  // columns to be displayed in table
+  displayedColumns: string[] = ['studentName', 'submission', 'submissionDate', 'grade', 'addGrade'];
 
   constructor(private teacherService: TeachersService, private activatedRoute: ActivatedRoute) {
     this.asnID = this.activatedRoute.snapshot.paramMap.get('asnID');
   }
-
+  // declares properties that go into submissions table
+  studentName: string;
+  submission: string;
+  submissionDate: any;
+  grade: number;
 
   ngOnInit() {
     this.viewOneAssignment();
@@ -36,9 +43,12 @@ export class TeachersAssignmentComponent implements OnInit {
   editAsnForm() {
     document.getElementById('editForm').style.display = 'block';
   }
+  closeForm() {
+    document.getElementById('editForm').style.display = 'none';
+  }
 
   editAsn() {
-    if (this.teacherService.editAssignment(this.asnID, this.asnTitle, this.asnBody, this.asnDueDate, this.assignment.asnGrade).subscribe(
+    if (this.teacherService.editAssignment(this.asnID, this.asnTitle, this.asnBody, this.asnDueDate, this.asnGrade).subscribe(
       () => this.viewOneAssignment()
     )) {
       document.getElementById('editForm').style.display = 'none';
@@ -50,6 +60,9 @@ export class TeachersAssignmentComponent implements OnInit {
         this.submissions = res;
       }
     );
+  }
+  addGrade() {
+    console.log('hi');
   }
 
 }
